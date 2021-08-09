69bee0e2cfedbc44196e65c54684ec8635ccef08
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-08-09 23:12:13 +0900

    feat : job detail page

---
+++ src/pages/DetailPage/DetailPage.js
@@ -0,0 +1,418 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import DetailSlider from "../DetailPage/DetailSlider/DetailSlider";
+import MapContent from "../DetailPage/Map/MapContent";
+import { DetailPage_API } from "../../config";
+import Detail from "./DetailData";
+import Navbar from "../../components/common/Navbar/navbar";
+import { PageWrap } from "../../components/common/styled";
+
+const DetailPage = () => {
+  // const [detail, setDetail] = useState({
+  //   id: 0,
+  //   title: '',
+  //   name: '',
+  //   response_rate: 0,
+  //   city: '',
+  //   compensation_recommender: 0,
+  //   compensation_applicant: 0,
+  //   likes_count: 0,
+  //   image_url: [],
+  //   contents: '',
+  //   deadline: '',
+  //   address: '',
+  //   location: [],
+  //   taglist: [],
+  //   color: "#dbdbdb",
+  //   button: false,
+  // })
+
+  const [detail, setDetail] = useState(Detail.job_detail);
+
+  // componentDidMount() {
+  //   fetch(`${DetailPage_API}/company/${props?.match.params.id}`, {
+  //     method: "GET"
+  //   })
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       if (res.message === "SUCCESS") {
+  //         this.setDetail({
+  //           detail: res.job_detail,
+  //           response_rate: res.job_detail.response_rate
+  //         });
+  //       }
+  //       if (this.state.response_rate > 50) {
+  //         this.setDetail({ button: true });
+  //       }
+  //     });
+  // }
+
+  const handleChangeColor = () => {
+    if (detail.color === "#fe415c") {
+      setDetail({ color: "#dbdbdb", likes_count: detail.likes_count - 1 });
+    } else {
+      setDetail({
+        color: "#fe415c",
+        likes_count: detail.likes_count + 1,
+      });
+    }
+  };
+
+  const handleChangeBtn = () => {
+    if (detail.response_rate > 50) {
+      setDetail({ button: true });
+    }
+  };
+
+
+  return (
+    <PageWrap>
+      <Navbar />
+      <DetailPageBox>
+        <DetailPageInner>
+          <div className="MainContents">
+            <div className="companyImg">
+              <DetailSlider imgUrl={detail.image_url} />
+            </div>
+            <div className="section_1">
+              <h2>{detail.title}</h2>
+              <div className="contents1">
+                <h6>{detail.name}</h6>
+                {detail.button && (
+                  <button onChange={handleChangeBtn}> 응답률높음</button>
+                )}
+                <h5>{detail.city}</h5>
+              </div>
+              <div className="tagArea">
+                <ul>
+                  {detail.tag_list &&
+                    detail.tag_list.map(tag => <li>{tag.name}</li>)}
+                </ul>
+              </div>
+            </div>
+            <div className="contents2">
+              <p>{detail.contents}</p>
+            </div>
+            <div className="section_2">
+              <div className="section_2Contents">
+                <div className="section_detail">
+                  <span className="header">마감일</span>
+                  <span className="body">{detail.deadline}</span>
+                </div>
+                <br></br>
+                <div className="section_detail">
+                  <span className="header">근무지역</span>
+                  <span className="body">{detail.address}</span>
+                </div>
+              </div>
+              <div className="map">
+                <MapContent location={detail && detail.location} />
+              </div>
+            </div>
+            <div className="section_3">
+              <h5 className="position">윈티드님, 이 포지션을 찾고 계셨나요?</h5>
+              <div className="companies"></div>
+            </div>
+          </div>
+          <div className="asideArea">
+            <div className="asideBox">
+              <h3>채용보상금</h3>
+              <ul>
+                <li>
+                  <h4>추천인</h4>
+                  <p>{detail.compensation_recommender}원</p>
+                </li>
+                <li>
+                  <h4>지원자</h4>
+                  <p>{detail.compensation_applicant}원</p>
+                </li>
+              </ul>
+            </div>
+            <div className="button">
+              <button className="bookMark">
+                <li class="far fa-bookmark"></li>
+                <li>북마크하기</li>
+              </button>
+              <ApplyButton>지원하기</ApplyButton>
+            </div>
+            <div className="likeArea">
+              <button className="likeCount">
+                <button
+                  onClick={handleChangeColor}
+                  style={{ color: detail.color }}
+                  className="fas fa-heart"
+                ></button>
+                <span>{detail.likes_count}</span>
+              </button>
+              <button className="likePeople">
+                <i class="far fa-user-circle"></i>
+              </button>
+            </div>
+          </div>
+        </DetailPageInner>
+      </DetailPageBox>
+    </PageWrap>
+  );
+
+}
+
+const DetailPageBox = styled.div`
+  width: 100%;
+  margin-top: 80px;
+`;
+
+const ApplyButton = styled.button`
+  width: 100%;
+  height: 50px;
+  border-radius: 25px;
+  border:none;
+  background-color: #36f;
+  font-size: 16px;
+  font-weight: 600;
+  color: #fff;
+  text-align: center;
+  cursor:pointer;
+`;
+
+const DetailPageInner = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
+  height: 100%;
+  margin: 0 auto;
+
+  .MainContents {
+    display:flex;
+    flex-direction:column;
+    width: 65%;
+    height: 100%;
+    .companyImg {
+      width: 100%;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .section_1 {
+      padding-top: 30px;
+      width: 100%;
+      h2 {
+        display: block;
+        color: #333;
+        font-size: 22px;
+        font-weight: 600;
+      }
+      .contents1 {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        h6 {
+          font-size: 13px;
+          padding-right: 10px;
+        }
+        button {
+          margin: 10px;
+          padding: 5px;
+          font-size: 12px;
+          background-color:#fff;
+          color: #36f;
+          border: 1px solid #36f;
+        }
+        h5 {
+          display: flex;
+          font-size: 15px;
+          color: #999;
+          display: inline-flex;
+          font-weight: 400;
+          padding-left: 10px;
+        }
+      }
+      .tagArea {
+        width: 100%;
+        display: flex;
+        ul {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          overflow: auto;
+          li {
+            margin-right: 6px;
+            padding: 9px 14px;
+            font-weight: 500;
+            font-size: 12px;
+            color: #333;
+            background-color: #f3f5f8;
+            border-radius: 25px;
+            list-style:none;
+          }
+        }
+      }
+    }
+    .contents2 {
+      width: 100%;
+
+      p {
+        line-height: 1.75;
+        font-size: 16px;
+        color: #333;
+        font-weight: 400;
+      }
+    }
+    .section_2 {
+      width: 100%;
+      display:flex;
+      flex-direction:column;
+      border-top: 1px solid #eeeeee;
+      margin: 20px 0;
+      .section_2Contents {
+        display:flex;
+        flex-direction:column;
+        .section_detail{
+          display:flex;
+          flex-direction:row;
+          width: content-fit;
+          align-items: center;
+          .header {
+          display:flex;
+          font-size: 16px;
+          font-weight: 600;
+          color: #999;
+          line-height: 40px;
+          margin-right:20px;
+          }
+        }
+        .body {
+          color: #333;
+          display: flex;
+          font-size: 16px;
+          font-weight: 600;
+        }
+      }
+      .map {
+        width: 100%;
+        height: 280px;
+      }
+    }
+    .section_3 {
+      margin: 80px 0 0;
+      .position {
+        margin: 0 0 20px;
+        font-size: 20px;
+        font-weight: 600;
+        color: #333;
+      }
+    }
+  }
+  .asideArea {
+    display:flex;
+    flex-direction:column;
+    position: fixed;
+    right: 15%;
+    width: 20%;
+    height: 40%;
+    border: 1px solid #e1e2e3;
+    background-color: #fff;
+    padding: 15px;
+    border-radius: 3px;
+    .asideBox {
+      width:100%;
+      margin-bottom:-5%;
+      padding:0;
+      h3 {
+        margin-bottom:-5%;
+        font-size: 15px;
+        font-weight: 600;
+        text-align: left;
+        color: #333;
+      }
+      ul {
+        display:flex;
+        flex-direction:row;
+        margin-left:-12%;
+        li {
+          width: 50%;
+          list-style:none;
+          text-align: left;
+          h4 {
+            font-size: 14px;
+            font-weight: 700;
+            color: #999;
+          }
+          p {
+            margin-top:-5%;
+            font-size: 16px;
+            color: #333;
+            font-weight: 800;
+          }
+        }
+      }
+    }
+    .button {
+      width:100%;
+      display:flex;
+      flex-direction:column;
+      cursor: pointer;
+      .bookMark {
+        cursor:pointer;
+        width: 100%;
+        height: 50px;
+        margin-bottom: 7px;
+        border-radius: 25px;
+        border: 1px solid #e1e2e3;
+        background-color: #fff;
+        display: flex;
+        justify-content: center;
+        li:first-child {
+          line-height: 48px;
+          color:#3366FF;
+        }
+        li:last-child {
+          line-height: 48px;
+          color: #3366FF;
+          font-size: 16px;
+          font-weight: 600;
+          text-align: center;
+          text-decoration: none;
+          list-style: none;
+          padding-left: 10px;
+        }
+      }
+    }
+    .likeArea {
+      display: flex;
+      flex-direction:row;
+      margin-top: 20px;
+      .likeCount {
+        height: 30px;
+        border-radius: 15px;
+        border: 1px solid #e1e2d3;
+        margin-right: 12px;
+        padding: 0 15px;
+        background-color: #fff;
+        cursor: pointer;
+        button {
+          cursor: pointer;
+          padding-right: 10px;
+          font-size: 15px;
+          background-color: #fff;
+          border: none;
+        }
+        span {
+          font-size: 15px;
+        }
+      }
+      .likePeople {
+        border:none;
+        background: #fff;
+        cursor: pointer;
+        i {
+          list-style:none;
+          font-size: 24px;
+        }
+      }
+    }
+  }
+`;
+
+export default DetailPage;
---
+++ src/pages/DetailPage/Map/MapContent.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+
+class MapContent extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      locationData: []
+      // markerPosition: {
+      //   lat: 0,
+      //   lng: 0
+      // }
+    };
+  }
+
+  // componentDidMount() {
+  //   const { location } = this.props.location;
+  //   console.log(">>>>>>>>>>", location);
+  //   this.setState(location => ({
+  //     markerPosition: {
+  //       ...location,
+  //       lat: Number(location[0]),
+  //       lng: Number(location[1])
+  //     }
+  //   }));
+  // }
+
+  renderMarkers = (map, maps) => {
+    const { markerPosition } = this.state;
+    let marker = new maps.Marker({
+      position: markerPosition,
+      map
+    });
+    return marker;
+  };
+
+  render() {
+    const mapStyles = {
+      width: "62%",
+      height: "90%"
+    };
+
+    const markerPosition = {
+      lat: Number(this.props.location ? this.props.location[0] : 0),
+      lng: Number(this.props.location ? this.props.location[1] : 0)
+    };
+
+    return (
+      <Map
+        google={this.props.google}
+        zoom={15}
+        style={mapStyles}
+        initialCenter={markerPosition}
+        onGoogleApiLoaded={({ map, maps }) => this.renderMarkers(map, maps)}
+        containerStyle={{ maxWidth: "1090px", height: "400px" }}
+      >
+        <Marker position={markerPosition} />
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyARVm_IFPx0fUSRsfdKAvKV9hBG5JWu6xw"
+})(MapContent);
